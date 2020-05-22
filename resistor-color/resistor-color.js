4a1cc37ef9917f396eadefaa2982a8b22cad966c
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-05-22 17:56:00 -0600

    Complete Resistor Color Challenge

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -1,10 +1,16 @@
-//
-// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white'
+];
 
-export const colorCode = () => {
-  throw new Error("Remove this statement and implement this function");
+export const colorCode = (nameColor) => {
+  return COLORS.indexOf(nameColor.toLowerCase());
 };
-
-export const COLORS = undefined;
