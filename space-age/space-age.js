0ddd8839e2c40dd3c282308dfd49bb19015119ed
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-05-28 14:47:42 -0600

    Complete Space Age Challenge

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -1,8 +1,15 @@
-//
-// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const PLANETS = {
+  earth:   1.0,
+  mercury: 0.2408467,
+  venus:   0.61519726,
+  mars:    1.8808158,
+  jupiter: 11.862615,
+  saturn:  29.447498,
+  uranus:  84.016846,
+  neptune: 164.79132
+}
 
-export const age = () => {
-  throw new Error("Remove this statement and implement this function");
+export const age = (planet, seconds) => {
+  var secondsPerYear = 31557600
+  return parseFloat((seconds / (secondsPerYear * PLANETS[planet])).toFixed(2))
 };
